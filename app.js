96415ea5fb3bfb7542eda958f13430e5bf284019
tranttxuan <tran.ttxuan91@gmail.com>
2020-12-28 01:54:37 +0100

    add animation for nav

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
+// INTRO ANIMATION
 function showElement(name) {
         document.querySelector(name).style.transform = `translate(0px,0px)`;
         document.querySelector(name).style.opacity = `1`;
 }
 
-function showList(name){
+function showList(name) {
         document.querySelectorAll(name).forEach((e, i) => {
                 let j = i;
                 setTimeout(() => {
                         e.style.transform = `translate(0px,0px)`;
                         e.style.opacity = `1`;
 
-                }, j*100);
+                }, j * 100);
         })
 }
 
@@ -55,4 +56,16 @@ window.onload = function () {
                 showList(".timeline .timeline-item")
         }, 5200);
 
+}
+
+// SCROLL
+window.onscroll = function () {
+        // console.log(window.pageYOffset)
+        if (window.pageYOffset > 5) {
+                console.log("check", window.pageYOffset)
+                document.querySelector(".nav").style.opacity = `0`;
+                document.querySelector(".nav").style.transition = `opacity 1s`;
+        }else{
+                document.querySelector(".nav").style.opacity = `initial`;
+        }
 }
